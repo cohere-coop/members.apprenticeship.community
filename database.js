146db3aa877494f220ccc1591963cd44447161c4
Wimsy113 <krystyna.d.l.ewing@gmail.com>
2015-12-09 21:42:54 -0500

    first pass at password encryption

--- database.js
+++ database.js
@@ -6,17 +6,35 @@ var Database = module.exports = function(connectionString) {
   }
   this.connectionString = connectionString
 }
+var bcrypt = require('bcrypt')
 
 Database.prototype.findUserByEmailAndPassword = function(email, password, callback) {
   this.query('SELECT id, email, password FROM users WHERE email=$1', [email],
              function(err, result) {
                  //use scrypt to compare the passwords here
+                 exports.comparePassword = function(password, userPassword, callback) {
+                     bcrypt.compare(password, userPassword, function(err, isPasswordMAtch) {
+                         if (err)
+                         return callback(err);
+                         return callback(null, isPasswordMAtch);
+                     });
+                 }
               callback(err, result.rows[0])
   });
 }
 
 Database.prototype.createUser = function(email, password, callback) {
     // here is where we should encrypt the password
+    exports.cryptPassword = function(password, userPassword, callback ) {
+        bcrypt.genSalt(10, function(err, salt) {
+            if (err)
+            return callback(err)
+
+            bcrypt.hash(password, salt, function(err, hash) {
+                return callback(err, hash)
+            })
+        })
+    }
  this.query("INSERT INTO users(id, email, password) VALUES (uuid_generate_v4(), $1, $2)", [email, password], function(err, result) {
     callback(err, result.rows)
   });
